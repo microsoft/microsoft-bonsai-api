d11e73d28c325d1f09f4e48bbbd05e3a154468da
Navvaran Mann <namann@microsoft.com>
2020-06-04 04:28:18 +0000

    Merged PR 9383: Add a couple tests + update stub server

--- Python/tests/test_client.py
+++ Python/tests/test_client.py
@@ -2,10 +2,125 @@
 Tests for BonsaiClient class
 Copyright 2020 Microsoft
 """
+from unittest.mock import Mock, patch
 
+import pytest
 from microsoft_bonsai_api.client import Config, BonsaiClient
+from azure.core.exceptions import HttpResponseError
 
 
 def test_default_client_construction():
     config = Config()
     BonsaiClient(config)
+
+
+# TODO: Test represents current behavor of library. It may not be what we want it to do
+def test_unauthorized_registration():
+    config = Config()
+    config.server = "http://127.0.0.1:9000"
+    config.workspace = "unauthorized"
+    config.access_key = "111"
+
+    client = BonsaiClient(config)
+
+    try:
+        client.create_session("a", 1, "c")
+    except HttpResponseError as err:
+        assert err.status_code == 401
+        assert "Unauthorized" in err.message
+
+
+# TODO: Test represents current behavor of library. It may not be what we want it to do
+def test_forbidden_registration():
+    config = Config()
+    config.server = "http://127.0.0.1:9000"
+    config.workspace = "forbidden"
+    config.access_key = "111"
+
+    client = BonsaiClient(config)
+
+    try:
+        client.create_session("a", 1, "c")
+    except HttpResponseError as err:
+        assert err.status_code == 403
+        assert "Forbidden" in err.message
+
+
+# TODO: Test represents current behavor of library. It may not be what we want it to do
+def test_badgateway_registration():
+    config = Config()
+    config.server = "http://127.0.0.1:9000"
+    config.workspace = "badgateway"
+    config.access_key = "111"
+
+    client = BonsaiClient(config)
+
+    try:
+        client.create_session("a", 1, "c")
+    except HttpResponseError as err:
+        assert err.status_code == 502
+        assert "Bad Gateway" in err.message
+
+
+# TODO: Test represents current behavor of library. It may not be what we want it to do
+def test_unavailable_registration():
+    config = Config()
+    config.server = "http://127.0.0.1:9000"
+    config.workspace = "unavailable"
+    config.access_key = "111"
+
+    client = BonsaiClient(config)
+
+    try:
+        client.create_session("a", 1, "c")
+    except HttpResponseError as err:
+        assert err.status_code == 503
+        assert "Unavailable" in err.message
+
+
+# TODO: Test represents current behavor of library. It may not be what we want it to do
+def test_gateway_timeout():
+    config = Config()
+    config.server = "http://127.0.0.1:9000"
+    config.workspace = "gatewaytimeout"
+    config.access_key = "111"
+
+    client = BonsaiClient(config)
+
+    try:
+        client.create_session("a", 1, "c")
+    except HttpResponseError as err:
+        assert err.status_code == 504
+        assert "Gateway Timeout" in err.message
+
+
+# TODO: Test represents current behavor of library. It may not be what we want it to do
+def test_training():
+    config = Config()
+    config.server = "http://127.0.0.1:9000"
+    config.workspace = "train"
+    config.access_key = "111"
+
+    client = BonsaiClient(config)
+    client.create_session("a", 1, "c")
+
+    counter = 0
+    while counter < 100:
+        client.advance("1", 1, {})
+        counter += 1
+
+
+@patch("time.sleep", return_value=None)
+def test_flaky_sim(patched_sleep: Mock):
+    config = Config()
+    config.server = "http://127.0.0.1:9000"
+    config.workspace = "flaky"
+    config.access_key = "111"
+
+    client = BonsaiClient(config)
+    client.create_session("a", 1, "c")
+
+    counter = 0
+    while counter < 100:
+        client.advance("1", 1, {})
+        counter += 1
--- Python/tests/web_server.py
+++ Python/tests/web_server.py
@@ -4,12 +4,29 @@ Copyright 2020 Microsoft
 """
 
 from aiohttp import web
+from .mock_responses import *
+
+
+def count_me(fnc):
+    """ decorator to count function calls in a class variable
+    """
+
+    def increment(self, *args, **kwargs):
+        self._count += 1
+        return fnc(self, *args, **kwargs)
+
+    return increment
 
 
 def get_app():
     stub = SimulatorGatewayStub()
     app = web.Application()
     app.router.add_get("/", stub.root)
+    app.router.add_post("/v2/workspaces/{workspace}/simulatorSessions", stub.register)
+    app.router.add_post(
+        "/v2/workspaces/{workspace}/simulatorSessions/{session_id}/advance",
+        stub.get_next_event,
+    )
     return app
 
 
@@ -19,11 +36,58 @@ def start_app() -> None:
 
 
 class SimulatorGatewayStub:
+    _FLAKY = False
+    _count = 0
+    _fail_point = 10
+    _fail_duration = 3
+
     def __init__(self):
         pass
 
+    def reset_flags(self):
+        self._FLAKY = False
+        self._count = 0
+
     async def root(self, request):
         return web.json_response({})
+
+    async def register(self, request):
+        if "unauthorized" in request.match_info["workspace"]:
+            return web.Response(status=401, text="Unauthorized")
+        if "forbidden" in request.match_info["workspace"]:
+            return web.Response(status=403, text="Forbidden")
+        if "badgateway" in request.match_info["workspace"]:
+            return web.Response(status=502, text="BadGateway")
+        if "unavailable" in request.match_info["workspace"]:
+            return web.Response(status=503, text="Service Unavailable")
+        if "gatewaytimeout" in request.match_info["workspace"]:
+            return web.Response(status=504, text="GatewayTimeout")
+        if "flaky" in request.match_info["workspace"]:
+            self._FLAKY = True
+        return web.json_response(status=201, data=MOCK_REGISTRATION_RESPONSE)
+
+    @count_me
+    async def get_next_event(self, request):
+        if (
+            self._FLAKY
+            and self._count > self._fail_point
+            and self._count < self._fail_point + self._fail_duration
+        ):
+            return web.Response(status=503, text="Service Unavailable")
+
+        if self._count == 1:
+            return web.json_response(MOCK_EPISODE_START_RESPONSE)
+
+        if self._count % 25 == 0:
+            return web.json_response(MOCK_EPISODE_FINISH_RESPONSE)
+
+        if "500" in request.match_info["workspace"]:
+            return web.json_response(status=500, text="Internal Server Error")
+
+        return web.json_response(MOCK_EPISODE_STEP_RESPONSE)
+
+    async def unregister(self, request):
+        return web.json_response(MOCK_UNREGISTER_RESPONSE)
 
 
 if __name__ == "__main__":

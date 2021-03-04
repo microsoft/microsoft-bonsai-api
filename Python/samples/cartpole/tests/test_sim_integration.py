ecbab2c575450010339c07241db992ccc1629f66
Ali Zaidi <alikaz.zaidi@gmail.com>
2021-03-03 21:46:18 -0800

    :bug: in tests: appended state in get_state for distance to target

--- Python/samples/cartpole/tests/test_sim_integration.py
+++ Python/samples/cartpole/tests/test_sim_integration.py
@@ -11,9 +11,7 @@ large_config = {"pole_mass": 0.055, "pole_length": 0.4, "cart_mass": 0.6}
 
 @pytest.fixture
 def sim():
-    from main import (
-        TemplateSimulatorSession,
-    )
+    from main import TemplateSimulatorSession
 
     sim = TemplateSimulatorSession(render=False)
     sim.episode_start(new_config)
@@ -59,11 +57,11 @@ def test_physics(sim):
 
     sim.episode_start(large_config)
     sim_state = sim.get_state()
-    # use the same action as above    
+    # use the same action as above
     sim.episode_step(random_action)
     next_state = sim.get_state()
     print(f"sim_state: {sim_state}; next_state: {next_state}")
-    
+
     smaller_delta_v = next_state["cart_velocity"] - sim_state["cart_velocity"]
 
     assert abs(smaller_delta_v) < abs(default_delta_v)
@@ -73,7 +71,7 @@ def test_sim_states(sim):
 
     sim_state = sim.get_state()
     assert type(sim_state) == dict
-    assert len(sim_state) == 10
+    assert len(sim_state) == 11
 
     for k, v in sim_state.items():
         assert isinstance(v, Number)

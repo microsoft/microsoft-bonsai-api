0ab4de2e53d1eca5bb7fa14b8c33c5360d7c22bb
Journey McDowell <jomcdowe@microsoft.com>
2021-04-27 17:28:16 -0700

    using number for action type and cleaning up halted

--- Python/samples/quanser-qube/main.py
+++ Python/samples/quanser-qube/main.py
@@ -105,10 +105,7 @@ class TemplateSimulatorSession:
         sim_state = self.get_state()
 
         # If arm hits rails of physical limit +/- 90 degrees
-        if abs(sim_state['theta']) > math.pi / 2:
-            return True
-        else:
-            return False
+        return abs(sim_state['theta']) >= math.pi / 2
 
     def log_iterations(self, state, action, episode: int = 0, iteration: int = 1):
         """Log iterations during training to a CSV.

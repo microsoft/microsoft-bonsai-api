73d770b4bac22f9ec8215f71949cdd4982d98d6b
David Coe <coedavid@umich.edu>
2022-05-19 12:30:11 -0400

    remove msal; msal_extension requirements

--- Python/setup.py
+++ Python/setup.py
@@ -30,9 +30,7 @@ setup(
     packages=find_namespace_packages(include=["microsoft_bonsai_api.*"]),
     install_requires=[
         "msrest>=0.6.0",
-        "azure-core<2.0.0,>=1.2.0",
-        "msal>=1.2.0,<=1.4.3",
-        "msal_extensions>=0.1.3,<=0.2.2",
+        "azure-core<2.0.0,>=1.2.0"
     ],
     test_suite="pytest",
     tests_require=["pytest>=5.4.2"],

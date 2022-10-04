466edc7b22e396ec34b31a32ca0a0907fe18bbac
John Alexander <johnalex@microsoft.com>
2022-10-04 15:40:03 -0700

    updated to match pattern

--- Python/samples/lunarlander/main.py
+++ Python/samples/lunarlander/main.py
@@ -15,14 +15,13 @@ from microsoft_bonsai_api.simulator.generated.models import (
     SimulatorSessionResponse,
 )
 from azure.core.exceptions import HttpResponseError
-
+from sim.simulator_model import SimulatorModel
 import gym
-from sim.LunarLander import LunarLander
+
 from policies import brain_policy, chosen_policy
 import argparse
 
 LOG_PATH = "logs"
-
 
 def random_policy(sim_state):
 
@@ -30,241 +29,6 @@ def random_policy(sim_state):
     env.reset()
     action = env.action_space.sample()
     return {"engine1": action[0], "engine2": action[1]}
-
-
-def ensure_log_dir(log_full_path: str):
-    """Create a log directory if needed
-
-    Parameters
-    ----------
-    log_full_path : str
-        path of log directory
-    """
-    print(f"Log filename: {log_full_path}")
-    logs_directory = pathlib.Path(log_full_path).parent.absolute()
-    print(f"Log directory: {logs_directory}")
-    if not pathlib.Path(logs_directory).exists():
-        print(
-            "Directory does not exist at {0}, creating now...".format(
-                str(logs_directory)
-            )
-        )
-        logs_directory.mkdir(parents=True, exist_ok=True)
-
-
-class TemplateSimulatorSession:
-    def __init__(
-        self,
-        render: bool = False,
-        log_data: bool = False,
-        log_file_name: str = None,
-        env_name: str = "LunarLanderContinuous-v2",
-        obs_type: str = "Kinematics", # TODO: Review if Kinematics is needed (config-related)
-        debug: bool = True,
-    ):
-        """Lunar Lander simulation using the gym environment. See API docs for more information:
-            https://www.gymlibrary.ml/environments/box2d/lunar_lander/
-
-        Parameters
-        ----------
-        render : bool, optional
-            render every control timestep of the environment, by default False
-        log_data : bool, optional
-            whether to MDP data to CSV, by default False
-        log_file_name : str, optional
-            where to log data, only applicable if `log_data=True`, by default None
-        env_name : str, optional
-            the name of the simulator environment to create, by default "highway-v0"
-        obs_type: str, optional
-            the type of observations received from simulator.
-        """
-
-        # Log data is disabled by default if using the logging from TemplateSimulatorSession.
-        self.sim = LunarLander(render = render,
-                               log_data = False, # Data disabled since it is taken care by this class.
-                               log_file_name = log_file_name,
-                               env_name = env_name,
-                               obs_type = obs_type,
-                               debug = debug)
-
-        # Logging features
-        self.log_data = log_data
-        if not log_file_name:
-            current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-            log_file_name = current_time + "_" + env_name + "_log.csv"
-
-        self.log_full_path = os.path.join(LOG_PATH, log_file_name)
-        ensure_log_dir(self.log_full_path)
-
-        self.debug = debug
-
-
-    def get_state(self) -> Dict[str, Any]:
-        """Extract state from MDP
-
-        Returns
-        -------
-        Dict[str, Any]
-            Get states from current simulator environment.
-        """
-
-        sim_state = self.sim.get_state()
-
-        state = {
-            "x_position": sim_state["x_position"],
-            "y_position": sim_state["y_position"],
-            "x_velocity": sim_state["x_velocity"],
-            "y_velocity": sim_state["y_velocity"],
-            "angle": sim_state["angle"],
-            "rotation": sim_state["rotation"],
-            "left_leg": sim_state["left_leg"],
-            "right_leg": sim_state["right_leg"],
-            # Converting to float due to parsing errors detecting during brain training
-            "ship_crashed": bool(sim_state["ship_crashed"]),
-            "ship_landed": bool(sim_state["ship_landed"]),
-            # Config Params
-            "randomized_strength": float(sim_state["randomized_strength"]),
-            "randomized_steps": float(sim_state["randomized_steps"]),
-            "delta_action": bool(sim_state["delta_action"]),
-            # Aux vars -- Useful for delta actions
-            "prev_engine1": float(sim_state["prev_engine1"]),
-            "prev_engine2": float(sim_state["prev_engine2"]),
-            # Gym reward/terminal
-            "gym_reward": float(sim_state["gym_reward"]),
-            "gym_terminal": bool(sim_state["gym_terminal"]),
-            # UNUSED AT THE MOMENT
-            "concept": float(sim_state["concept"]),
-        }
-
-        return state
-
-    def episode_start(self, config: Union[Dict[str, Any], None] = None):
-        """Initialize a new episode of the simulator with configuration parameters.
-
-        Parameters
-        ----------
-        config : Dict[str, Any] or None
-            paramters for initializing the simulator. See 'sim/LunarLander.py' to see all config parameters.
-        """
-        
-        # Start simulation.
-        self.sim.episode_start(config)
-
-        # Logging functionality.
-        if self.log_data:
-            action = {"engine1": None, "engine2": None}
-            self.log_iterations(state=self.get_state(),
-                                action=action,
-                                episode=self.sim.episode,
-                                iteration=self.sim.iteration)
-        
-        
-
-    def episode_step(self, action: Dict[str, Any]):
-        """Step through the environment. See allowable actions here: https://highway-env.readthedocs.io/en/latest/actions/index.html
-
-        Parameters
-        ----------
-        action : Dict[str, Any]
-            Control/action to iterate in the environment
-        """
-        
-        # Apply action to sim.
-        self.sim.episode_step(action)
-
-        # Logging functionality.
-        if self.log_data:
-            self.log_iterations(state=self.get_state(),
-                                action=action,
-                                episode=self.sim.episode,
-                                iteration=self.sim.iteration)
-        
-
-    def halted(self) -> bool:
-        """Terminate the episode if and only if the simulator cannot be continued. This washes away the entire episode from training.
-        """
-
-        return False
-
-    @property
-    def terminal(self):
-        return self.sim.terminal
-
-    @property
-    def env_name(self):
-        return self.sim.env_name
-
-    def log_iterations(self, state, action, episode: int = 0, iteration: int = 1):
-        """Log iterations during training to a CSV.
-
-        Parameters
-        ----------
-        state : Dict
-        action : Dict
-        episode : int, optional
-        iteration : int, optional
-        """
-
-        import pandas as pd
-
-        def add_prefixes(d, prefix: str):
-            return {f"{prefix}_{k}": v for k, v in d.items()}
-        
-        # Custom way to turn lists into strings for logging
-        log_state = state.copy()
-        
-        for key, value in log_state.items():
-            if type(value) == list:
-                log_state[key] = str(log_state[key])
-
-        log_state = add_prefixes(log_state, "state")
-        action = add_prefixes(action, "action")
-        config = add_prefixes(self.sim.config, "config")
-        data = {**log_state, **action, **config}
-        data["episode"] = episode
-        data["iteration"] = iteration
-        log_df = pd.DataFrame(data, index=[0])
-
-        if os.path.exists(self.log_full_path):
-            log_df.to_csv(
-                path_or_buf=self.log_full_path, mode="a", header=False, index=False
-            )
-        else:
-            log_df.to_csv(
-                path_or_buf=self.log_full_path, mode="w", header=True, index=False
-            )
-
-
-def env_setup(env_file: str = ".env"):
-    """Helper function to setup connection with Project Bonsai
-
-    Returns
-    -------
-    Tuple
-        workspace, and access_key
-    """
-
-    load_dotenv(dotenv_path=env_file, verbose=True, override=True)
-    workspace = os.getenv("SIM_WORKSPACE")
-    access_key = os.getenv("SIM_ACCESS_KEY")
-
-    env_file_exists = os.path.exists(env_file)
-    if not env_file_exists:
-        open(env_file, "a").close()
-
-    if not all([env_file_exists, workspace]):
-        workspace = input("Please enter your workspace id: ")
-        set_key(env_file, "SIM_WORKSPACE", workspace)
-    if not all([env_file_exists, access_key]):
-        access_key = input("Please enter your access key: ")
-        set_key(env_file, "SIM_ACCESS_KEY", access_key)
-
-    load_dotenv(dotenv_path=env_file, verbose=True, override=True)
-    workspace = os.getenv("SIM_WORKSPACE")
-    access_key = os.getenv("SIM_ACCESS_KEY")
-
-    return workspace, access_key
-
 
 def test_policy(
     render: bool = False,
@@ -311,11 +75,9 @@ def test_policy(
     num_episodes = len(scenario_configs) + 1
 
     current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    log_file_name = current_time + "_" + policy_name + "_log.csv"
-    sim_session = TemplateSimulatorSession(
+    sim_session = SimulatorModel(
         render=render,
         log_data=log_iterations,
-        log_file_name=log_file_name,
         debug=debug
     )
     for episode in range(1, num_episodes):
@@ -340,14 +102,10 @@ def test_policy(
 
     return sim_session
 
-
 def main(
     render: bool = False,
     log_iterations: bool = False,
-    config_setup: bool = False,
-    env_file: Union[str, bool] = ".env",
-    workspace: str = None,
-    accesskey: str = None,
+    debug: bool = False,
 ):
     """Main entrypoint for running simulator connections
 
@@ -367,42 +125,14 @@ def main(
         optional flag from CLI for accesskey to override
     """
 
-    # check if workspace or access-key passed in CLI
-    use_cli_args = all([workspace, accesskey])
-
-    # use dotenv file if provided
-    use_dotenv = env_file or config_setup
-
-    # check for accesskey and workspace id in system variables
-    # Three scenarios
-    # 1. workspace and accesskey provided by CLI args
-    # 2. dotenv provided
-    # 3. system variables
-    # do 1 if provided, use 2 if provided; ow use 3; if no sys vars or dotenv, fail
-
-    if use_cli_args:
-        # BonsaiClientConfig will retrieve as environment variables
-        os.environ["SIM_WORKSPACE"] = workspace
-        os.environ["SIM_ACCESS_KEY"] = accesskey
-    elif use_dotenv:
-        if not env_file:
-            env_file = ".env"
-        print(
-            f"No system variables for workspace-id or access-key found, checking in env-file at {env_file}"
+    try: 
+        workspace = os.environ["SIM_WORKSPACE"]
+        accesskey = os.environ["SIM_ACCESS_KEY"]
+    except:
+        raise IndexError(
+            f"Workspace or access key not set or found. Use --config-setup for help setting up."
         )
-        workspace, accesskey = env_setup(env_file)
-        load_dotenv(env_file, verbose=True, override=True)
-    else:
-        try:
-            workspace = os.environ["SIM_WORKSPACE"]
-            accesskey = os.environ["SIM_ACCESS_KEY"]
-        except:
-            raise IndexError(
-                f"Workspace or access key not set or found. Use --config-setup for help setting up."
-            )
 
-    # Grab standardized way to interact with sim API
-    sim = TemplateSimulatorSession(render=render, log_data=log_iterations)
 
     # Configure client to interact with Bonsai service
     config_client = BonsaiClientConfig()
@@ -410,53 +140,35 @@ def main(
 
     # Create simulator session and init sequence id
     registration_info = SimulatorInterface(
-        name=sim.env_name,
+        name="LunarLanderContinuous-v2",
         timeout=60,
         simulator_context=config_client.simulator_context,
     )
 
-    def CreateSession(
-        registration_info: SimulatorInterface, config_client: BonsaiClientConfig
-    ):
-        """Creates a new Simulator Session and returns new session, sequenceId
-        """
+    print(
+        "config: {}, {}".format(config_client.server, config_client.workspace)
+    )
+    registered_session: SimulatorSessionResponse = client.session.create(
+        workspace_name=config_client.workspace, body=registration_info
+    )
+    print("Registered simulator. {}".format(registered_session.session_id))
 
-        try:
-            print(
-                "config: {}, {}".format(config_client.server, config_client.workspace)
-            )
-            registered_session: SimulatorSessionResponse = client.session.create(
-                workspace_name=config_client.workspace, body=registration_info
-            )
-            print("Registered simulator. {}".format(registered_session.session_id))
-
-            return registered_session, 1
-        except HttpResponseError as ex:
-            print(
-                "HttpResponseError in Registering session: StatusCode: {}, Error: {}, Exception: {}".format(
-                    ex.status_code, ex.error.message, ex
-                )
-            )
-            raise ex
-        except Exception as ex:
-            print(
-                "UnExpected error: {}, Most likely, it's some network connectivity issue, make sure you are able to reach bonsai platform from your network.".format(
-                    ex
-                )
-            )
-            raise ex
-
-    registered_session, sequence_id = CreateSession(registration_info, config_client)
     episode = 0
     iteration = 0
-
+    sim_model = SimulatorModel(
+        render=render,
+        log_data=log_iterations,
+        debug=debug
+    )
+    sim_model_state = { 'sim_halted': False }
     try:
         while True:
             # Advance by the new state depending on the event type
             # TODO: it's risky using `get_state` without first initializing the sim
             sim_state = SimulatorState(
-                sequence_id=sequence_id, state=sim.get_state(), halted=sim.halted(),
-            )
+                sequence_id=sequence_id, state=sim_model_state, halted=sim_model_state.get('sim_halted', False)
+                )
+    
             try:
                 event = client.session.advance(
                     workspace_name=config_client.workspace,
@@ -473,20 +185,9 @@ def main(
                         ex.status_code, ex.error.message, ex
                     )
                 )
-                # This can happen in network connectivity issue, though SDK has retry logic, but even after that request may fail,
-                # if your network has some issue, or sim session at platform is going away..
-                # So let's re-register sim-session and get a new session and continue iterating. :-)
-                registered_session, sequence_id = CreateSession(
-                    registration_info, config_client
-                )
                 continue
             except Exception as err:
                 print("Unexpected error in Advance: {}".format(err))
-                # Ideally this shouldn't happen, but for very long-running sims It can happen with various reasons, let's re-register sim & Move on.
-                # If possible try to notify Bonsai team to see, if this is platform issue and can be fixed.
-                registered_session, sequence_id = CreateSession(
-                    registration_info, config_client
-                )
                 continue
 
             # Event loop
@@ -495,14 +196,17 @@ def main(
                 print("Idling...")
             elif event.type == "EpisodeStart":
                 print("Episode config being used:", event.episode_start.config)
-                sim.episode_start(event.episode_start.config)
+                sim_model_state = sim_model.reset(event.episode_start.config)
+                print(f"state {sim_model_state}")
                 episode += 1
             elif event.type == "EpisodeStep":
                 iteration += 1
-                sim.episode_step(event.episode_step.action)
-                # Logging happens directly within TemplateSimulatorSession.
+                print(f"action {event.episode_step.action}")
+                sim_model_state = sim_model.step(event.episode_step.action)
+                print(f"state {sim_model_state}")
             elif event.type == "EpisodeFinish":
                 print("Episode Finishing...")
+                sim_model_state = { 'sim_halted': False }
                 iteration = 0
             elif event.type == "Unregister":
                 print(
@@ -510,12 +214,8 @@ def main(
                         event.unregister.details
                     )
                 )
-                registered_session, sequence_id = CreateSession(
-                    registration_info, config_client
-                )
-                continue
-            else:
-                pass
+                return
+
     except KeyboardInterrupt:
         # Gracefully unregister with keyboard interrupt
         client.session.delete(
@@ -547,31 +247,9 @@ if __name__ == "__main__":
         help="Log iterations during training",
     )
     parser.add_argument(
-        "--config-setup",
-        action="store_true",
+        "--debug",
         default=False,
-        help="Use a local environment file to setup access keys and workspace ids",
-    )
-    parser.add_argument(
-        "--env-file",
-        type=str,
-        metavar="ENVIRONMENT FILE",
-        help="path to your environment file",
-        default=None,
-    )
-    parser.add_argument(
-        "--workspace",
-        type=str,
-        metavar="WORKSPACE ID",
-        help="your workspace id",
-        default=None,
-    )
-    parser.add_argument(
-        "--accesskey",
-        type=str,
-        metavar="Your Bonsai workspace access-key",
-        help="your bonsai workspace access key",
-        default=None,
+        help="Debug sim using default embedded methods in sim.py",
     )
 
     group = parser.add_mutually_exclusive_group()
@@ -646,8 +324,6 @@ if __name__ == "__main__":
             config_setup=args.config_setup,
             render=args.render,
             log_iterations=args.log_iterations,
-            env_file=args.env_file,
-            workspace=args.workspace,
-            accesskey=args.accesskey,
+            debug=args.debug,
         )
 
---
+++ Python/samples/lunarlander/sim/simulator_model.py
@@ -0,0 +1,50 @@
+from LunarLander import LunarLander
+from typing import NamedTuple, Dict, Any
+
+
+class SimulatorModel:
+    """
+    Manages the LunarLander model that represents the simulation for this sample.
+    Implements the reset and step methods required for a Bonsai simulator.
+    """
+
+    def __init__(self,
+        render: bool = False,
+        log_data: bool = False,
+        debug: bool = True,):
+        """ Perform global initialization here if needed before running episodes. """
+
+        # render functionality
+        self.render = render
+        # logging features
+        self.log_data = log_data
+        # debug functionality
+        self.debug = debug
+
+        # initialize sim
+        self.sim = LunarLander(render = self.render,
+                               log_data = self.log_data, # Data disabled since it is taken care by this class.
+                               debug = self.debug)
+        
+        pass
+
+    def reset(self, config) -> Dict[str, Any]:
+        """ Reset any state from the previous episode and get ready to start a new episode. """
+        self.sim = LunarLander(render = self.render,
+                               log_data = self.log_data, # Data disabled since it is taken care by this class.
+                               debug = self.debug)
+                
+        # Start simulation.
+        self.sim.episode_start(config)
+
+        return self.sim.get_state()                        
+
+    def step(self, action) -> Dict[str, Any]:
+        """ Apply the specified action and perform one simulation step. """
+        # Apply action to sim.
+        self.sim.episode_step(action)
+
+        # If 'sim_halted' is set to True, that indicates that the simulator is unable to continue and
+        # the episode will be discarded. This simulator sets it to False because it can always continue.
+        return self.sim.get_state()
+

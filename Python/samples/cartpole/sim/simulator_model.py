5907ae0dc2fec9d0b4647fe2d1aafa75b4f586e0
Chris Kahrs <chriskahrs@msn.com>
2023-02-07 14:52:03 -0500

    changed simple adder to cartpole and tested

--- Python/samples/cartpole/sim/simulator_model.py
+++ Python/samples/cartpole/sim/simulator_model.py
@@ -1,24 +1,26 @@
-from sim.adder import Adder
+from sim.cartpole import CartPole
 from typing import NamedTuple, Dict, Any
 
 class SimulatorModel:
     """
-    Manages the Adder that represents the simulation for this sample.
+    Manages the CartPole that represents the simulation for this sample.
     Implements the reset and step methods required for a Bonsai simulator.
     """
 
     def __init__(self):
         """ Perform global initialization here if needed before running episodes. """
-        pass
+        self.cartpole = CartPole()
 
     def reset(self, config) -> Dict[str, Any]:
         """ Reset any state from the previous episode and get ready to start a new episode. """
-        self.adder = Adder(config['initial_value'])
-        return { 'sim_halted': False, 'value': self.adder.value }
-
+        self.cartpole.reset(**config)
+        state = self.cartpole.state.copy()
+        state['sim_halted'] = False
+        return state
+    
     def step(self, action) -> Dict[str, Any]:
         """ Apply the specified action and perform one simulation step. """
-        self.adder.add(action['addend'])
-        # If 'sim_halted' is set to True, that indicates that the simulator is unable to continue and
-        # the episode will be discarded. This simulator sets it to False becasue it can always continue.
-        return { 'sim_halted': False, 'value': self.adder.value }
+        self.cartpole.step(action['command'])
+        state = self.cartpole.state.copy()
+        state['sim_halted'] = False
+        return state
